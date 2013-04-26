2988ef611970b7baa48cbeb6a44928081d03d5eb
Jimi Ford <JimiHFord@users.noreply.github.com>
2013-04-26 17:44:51 -0400

    Added a small trainer class to represent Trainers

--- src/data/DataFetch.java
+++ src/data/DataFetch.java
@@ -10,6 +10,18 @@ package data;
 public class DataFetch {
 
 	
+	private static class SingletonWrapper {
+		public static DataFetch INSTANCE = new DataFetch();
+	}
+	
+	private DataFetch() {	}
+	
+	public static DataFetch getInstance() {
+		return SingletonWrapper.INSTANCE;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("This should compile.");
 	}
