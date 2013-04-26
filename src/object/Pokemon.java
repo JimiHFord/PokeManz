baba6700abd12a1384d1aa5672550b1caa4e3352
Jimi Ford <jimifordmail@gmail.com>
2013-04-26 18:02:00 -0400

    Added a Pokemon class to temporarily store data for Pokemanz.

---
+++ src/object/Pokemon.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package object;
+
+/**
+ * @author jimiford
+ *
+ */
+public class Pokemon {
+
+	private int id;
+	private String name;
+	
+	public Pokemon(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+}
