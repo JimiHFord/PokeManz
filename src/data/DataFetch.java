a74b988933dfd205b9a5fa8f1b7754b6e822a3ad
Ryan Castner <rrc9704@rit.edu>
2013-04-26 17:38:12 -0400

    committing

--- src/data/DataFetch.java
+++ src/data/DataFetch.java
@@ -5,7 +5,7 @@ package data;
 
 /**
  * @author jimiford
- *
+ * @deprecated your mom
  */
 public class DataFetch {
 
@@ -14,5 +14,6 @@ public class DataFetch {
 		System.out.println("This should compile.");
 		//or should it?
 		/*yes, it should*/
+		//ryan commit test xD
 	}
 }
