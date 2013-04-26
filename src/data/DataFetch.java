c15beb48c698380d1ae4aef4a224163bdc2cb888
Jimi Ford <jimifordmail@gmail.com>
2013-04-26 17:29:34 -0400

    Initial commit. This is very hard to see.

---
+++ src/data/DataFetch.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package data;
+
+/**
+ * @author jimiford
+ *
+ */
+public class DataFetch {
+
+	
+	public static void main(String[] args) {
+		System.out.println("This should compile.");
+	}
+}
