be6ffef098264f604635a3e6bcf23eecd5b0f681
JimiHFord <jhf3617@g.rit.edu>
2013-04-29 16:51:45 -0400

    Started implementing team panel.

--- src/view/pokeparty/TeamPanel.java
+++ src/view/pokeparty/TeamPanel.java
@@ -3,6 +3,8 @@
  */
 package view.pokeparty;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 
 import data.DataFetch;
@@ -14,16 +16,32 @@ import data.DataFetch;
 @SuppressWarnings("serial")
 public class TeamPanel extends JPanel {
 
+	private JPanel right;
 	private DataFetch df;
 	private String user;
-	
+	private ExplicitPartyScrollPane partyScrollPane;
 	
 	public TeamPanel(DataFetch df) {
+		super(new BorderLayout());
 		this.df = df;
+		this.user = "";
+		this.refresh();
+		this.right = new JPanel(new BorderLayout());
+		this.fillComponents();
 	}
 
+	public void fillComponents() {
+		right.add(partyScrollPane, BorderLayout.SOUTH);
+		this.add(right, BorderLayout.EAST);
+	}
+	
 	public void setUser(String user) {
-		this.user = user;		
+		this.user = user;
+		this.refresh();
+	}
+	
+	public void refresh() {
+		this.partyScrollPane = new ExplicitPartyScrollPane(df,user);
 	}
 	
 	
