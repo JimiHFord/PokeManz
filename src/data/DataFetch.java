9962665ee742ba30f2437d2cc00c68c6cce1b0a4
Ryan Castner <rrc9704@rit.edu>
2013-05-03 21:31:27 -0400

    fixed pattern matching in getPokevolveQuery() method

--- src/data/DataFetch.java
+++ src/data/DataFetch.java
@@ -344,7 +344,7 @@ public class DataFetch {
 	
 	public ArrayList<String> getPokevolveQuery(String search){
 		ArrayList<String> queryData = new ArrayList<String>();
-		if(search.matches("[0-9]")){
+		if(search.matches("[a-zA-Z]+") == false){
 			try{
 				ResultSet rs = stmt.executeQuery("select * from v_evolution_data where national_id = " + search + ";");
 				while(rs.next()){
