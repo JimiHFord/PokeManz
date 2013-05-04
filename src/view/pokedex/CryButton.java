17f369cbeace791062dc0dae9bf0d11183f7081e
Jimi Ford <jimifordmail@gmail.com>
2013-05-04 12:40:05 -0400

    Adding cry button and sound playing functionality.

---
+++ src/view/pokedex/CryButton.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package view.pokedex;
+
+import javax.swing.JButton;
+
+/**
+ * @author jimiford
+ *
+ */
+@SuppressWarnings("serial")
+public class CryButton extends JButton {
+
+	private int national_id;
+	
+	public CryButton(int national_id) {
+		super("Cry");
+		setNationalID(national_id);
+	}
+	
+	public void setNationalID(int national_id) {
+		this.national_id = national_id;
+	}
+	
+	public int getNationalID() {
+		return this.national_id;
+	}
+}
