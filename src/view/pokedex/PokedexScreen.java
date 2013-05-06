5800ac6c9314fb9975b7786ffd832dab4ab686bc
Jimi Ford <jimifordmail@gmail.com>
2013-05-05 21:06:01 -0400

    Added a slider to control volume of cry on PokedexScreen.

--- src/view/pokedex/PokedexScreen.java
+++ src/view/pokedex/PokedexScreen.java
@@ -19,15 +19,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Port;
+import javax.sound.sampled.Port.Info;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSlider;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import view.GUIEntryPoint;
 import view.PokeListener;
@@ -50,7 +58,7 @@ public class PokedexScreen extends JPanel implements PokeListener {
 	private static final String IMG_PATH = RESOURCE_DIR + "images/";
 	public static final String DISABLE = "DISABLE";
 	public static final String ENABLE = "ENABLE";
-	
+
 	private String pokemon;	
 	private DataFetch df;
 	private JTextArea jta;
@@ -77,17 +85,18 @@ public class PokedexScreen extends JPanel implements PokeListener {
 	private JButton pokemetricsBtn;
 	private JButton pokevolveBtn;
 	private JScrollPane jsp;
+	private JSlider volume;
 	private ArrayList<String> dexData;
 	private PokeListener listener;
 	private PokeListener thisListener = this;
-	
+
 	public PokedexScreen(PokeListener listener){
 		super(new MigLayout());
 		this.listener = listener;
 		this.df = DataFetch.getInstance();
 		initComponents();
 	}
-	
+
 	private void initComponents(){
 		jta = new JTextArea(DEFAULT);
 		jta.setPreferredSize(new Dimension(160,25));
@@ -113,6 +122,7 @@ public class PokedexScreen extends JPanel implements PokeListener {
 		ability1 = new JLabel("");
 		ability2 = new JLabel("");
 		imageLbl = new JLabel("");
+		volume = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
 		pokemetricsBtn = new JButton();
 		pokemetricsBtn.setVisible(false);
 		pokevolveBtn = new JButton();
@@ -147,12 +157,13 @@ public class PokedexScreen extends JPanel implements PokeListener {
 		southPanel.add(ability2, "wrap");
 		southPanel.add(cryLabel);
 		southPanel.add(cry, "wrap");
+		southPanel.add(volume);
 		southPanel.add(pokemetricsBtn, "gaptop 50");
 		southPanel.add(pokevolveBtn, "gaptop 50");
 		this.add(southPanel, "south");
 		this.setPokedexEntry("Bulbasaur", 1);
 	}
-	
+
 	private void updateTable(){
 		if(jta.getText().equals(DEFAULT)){
 			this.table.setModel(df.getSimplifiedDefaultPokemonModel());
@@ -168,7 +179,7 @@ public class PokedexScreen extends JPanel implements PokeListener {
 			this.table.getColumnModel().getColumn(1).setMaxWidth(100);
 		}
 	}
-	
+
 	private void updatePokedexName(){
 		String search = "" + pokemon;
 		dexData = df.getPokedexQuery(search);
@@ -188,56 +199,51 @@ public class PokedexScreen extends JPanel implements PokeListener {
 		try {
 			imgs = ImageIO.read(new java.io.File(path));
 		} catch (IOException e) {	}
-		
+
 		Image resizeImg = imgs == null ? 
 				img.getScaledInstance(200, 200, Image.SCALE_SMOOTH) :
-				img.getScaledInstance(imgs.getWidth()/2, imgs.getHeight()/2, Image.SCALE_SMOOTH);
-		imageLbl.setIcon(new ImageIcon(resizeImg));
-		nameLbl.setText(dexData.get(1));
-		nameLbl.setFont(new Font("Dialog", Font.BOLD, 26));
-		height.setText(dexData.get(2) + " m");
-		weight.setText(dexData.get(3) + " kg");
-		species.setText(dexData.get(4));
-		color.setText(dexData.get(5));
-		type1.setText(dexData.get(6));
-		type2.setText(dexData.get(7));
-		ability1.setText(dexData.get(8));
-		ability2.setText(dexData.get(9));
-		htLbl.setText("Height:");
-		wtLbl.setText("Weight:");
-		spcsLbl.setText("Species:");
-		colorLbl.setText("Color:");
-		if(type1.getText().equals(type2.getText())){
-			type2.setText("");
-			typesLbl.setText("Type:");
-		}else{
-			typesLbl.setText("Types:");
-		}
-		if(ability2.getText().equals("None")){
-			ability2.setText("");
-			abilitiesLbl.setText("Ability:");
-		}else{
-			abilitiesLbl.setText("Abilities:");
-		}
-		pokemetricsBtn.setText("Pokemetrics");
-		pokemetricsBtn.setVisible(true);
-		pokevolveBtn.setText("Pokevolve");
-		pokevolveBtn.setVisible(true);
-		southPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		this.revalidate();
+					img.getScaledInstance(imgs.getWidth()/2, imgs.getHeight()/2, Image.SCALE_SMOOTH);
+				imageLbl.setIcon(new ImageIcon(resizeImg));
+				nameLbl.setText(dexData.get(1));
+				nameLbl.setFont(new Font("Dialog", Font.BOLD, 26));
+				height.setText(dexData.get(2) + " m");
+				weight.setText(dexData.get(3) + " kg");
+				species.setText(dexData.get(4));
+				color.setText(dexData.get(5));
+				type1.setText(dexData.get(6));
+				type2.setText(dexData.get(7));
+				ability1.setText(dexData.get(8));
+				ability2.setText(dexData.get(9));
+				htLbl.setText("Height:");
+				wtLbl.setText("Weight:");
+				spcsLbl.setText("Species:");
+				colorLbl.setText("Color:");
+				if(type1.getText().equals(type2.getText())){
+					type2.setText("");
+					typesLbl.setText("Type:");
+				}else{
+					typesLbl.setText("Types:");
+				}
+				if(ability2.getText().equals("None")){
+					ability2.setText("");
+					abilitiesLbl.setText("Ability:");
+				}else{
+					abilitiesLbl.setText("Abilities:");
+				}
+				pokemetricsBtn.setText("Pokemetrics");
+				pokemetricsBtn.setVisible(true);
+				pokevolveBtn.setText("Pokevolve");
+				pokevolveBtn.setVisible(true);
+				southPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+				this.revalidate();
 	}
-	
-//	public void setPokedexEntry(String pokemon){
-//		this.pokemon = pokemon;
-//		updatePokedexName();
-//	}
-	
+
 	public void setPokedexEntry(String pokemon, int national_id) {
 		this.pokemon = pokemon;
 		this.cry.setNationalID(national_id);
 		updatePokedexName();
 	}
-	
+
 	private void initializeActions(){
 		this.jta.addFocusListener(new FocusListener(){
 			public void focusGained(FocusEvent e){
@@ -261,7 +267,7 @@ public class PokedexScreen extends JPanel implements PokeListener {
 				}
 				updateTable();
 			}
-			
+
 		});
 		this.pokemetricsBtn.addActionListener(new ActionListener(){
 
@@ -276,7 +282,7 @@ public class PokedexScreen extends JPanel implements PokeListener {
 			public void actionPerformed(ActionEvent e) {
 				listener.act(GUIEntryPoint.POKEVOLVE, pokemon);
 			}
-			
+
 		});
 		this.table.addMouseListener(new MouseListener(){
 
@@ -292,25 +298,25 @@ public class PokedexScreen extends JPanel implements PokeListener {
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseExited(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 		this.cry.addActionListener(new ActionListener() {
@@ -319,9 +325,32 @@ public class PokedexScreen extends JPanel implements PokeListener {
 				new SoundJLayer(SOUND_DIR + cry.getNationalID() + EXT, thisListener).play();
 			}
 		});
+		this.volume.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				if(!volume.getValueIsAdjusting()) {
+					Info source = Port.Info.SPEAKER;
+					if (AudioSystem.isLineSupported(source)) {
+						try {
+							Port outline = (Port) AudioSystem.getLine(source);
+							outline.open();                
+							FloatControl volumeControl = (FloatControl) outline.getControl(FloatControl.Type.VOLUME);                
+//							System.out.println("       volume: " + volumeControl.getValue() );
+							float v = volume.getValue()/100f;
+							volumeControl.setValue(v);
+//							System.out.println("   new volume: " + volumeControl.getValue() );
+							outline.close();
+						} catch (LineUnavailableException ex) {
+							System.err.println("Audio system not supported.");
+							volume.setEnabled(false);
+						} 
+					}
+				} 
+			}			
+		});
 	}
-	
-	
+
+
 	@Override
 	public void act(String command, String argument) {
 		switch(command) {
@@ -335,10 +364,10 @@ public class PokedexScreen extends JPanel implements PokeListener {
 			System.err.println("Internal error. Wrong message sent.");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new SoundJLayer(SOUND_DIR + 490 + EXT).play();
 	}
 
-	
+
 }
--- src/view/pokedex/SoundJLayer.java
+++ src/view/pokedex/SoundJLayer.java
@@ -3,6 +3,12 @@
  */
 package view.pokedex;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Port;
+import javax.sound.sampled.Port.Info;
+
 import view.PokeListener;
 import javazoom.jl.player.advanced.AdvancedPlayer;
 import javazoom.jl.player.advanced.PlaybackEvent;
@@ -88,5 +94,34 @@ public class SoundJLayer extends PlaybackListener implements Runnable
 		}
 
 	}
+	
+	public static void main(String[] args) 
+	{        
+	    Info source = Port.Info.SPEAKER;
+	    //        source = Port.Info.LINE_OUT;
+	    //        source = Port.Info.HEADPHONE;
+
+	        if (AudioSystem.isLineSupported(source)) 
+	        {
+	            try 
+	            {
+	                Port outline = (Port) AudioSystem.getLine(source);
+	                outline.open();                
+	                FloatControl volumeControl = (FloatControl) outline.getControl(FloatControl.Type.VOLUME);                
+	                System.out.println("       volume: " + volumeControl.getValue() );
+	                float v = 0.33F;
+	                volumeControl.setValue(v);
+	                System.out.println("   new volume: " + volumeControl.getValue() );
+	                v = 0.73F;
+	                volumeControl.setValue(v); 
+	                System.out.println("newest volume: " + volumeControl.getValue() );
+	            } 
+	            catch (LineUnavailableException ex) 
+	            {
+	                System.err.println("source not supported");
+	                ex.printStackTrace();
+	            }            
+	        }
+	    } 
 
 }
