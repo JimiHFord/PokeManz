6d51fc5451fd0b42d69b1ccf009a6068207d27ed
Felipe <fmc3147@g.rit.edu>
2013-05-08 22:10:51 -0400

    Documentation

--- src/view/PokeListener.java
+++ src/view/PokeListener.java
@@ -5,6 +5,13 @@ public interface PokeListener {
 	
 	public static final String TAB_VIEW = "TAB_VIEW";
 	public static final String TRAINER_VIEW = "TRAINER_VIEW";	
+	
+	/**
+	 * Child actors call this method with instructions
+	 * for parent to follow to update itself
+	 * @param command
+	 * @param argument
+	 */
 	public void act(String command, String argument);
 
 }
